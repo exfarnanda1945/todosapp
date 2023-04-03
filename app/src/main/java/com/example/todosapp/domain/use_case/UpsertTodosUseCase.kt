620847708654ea1e1dbaf7bb7f2790b8be2a2a6c
exfarnanda1945 <exfarnanda1945@gmail.com>
2023-04-03 10:16:08 +0700

    add upsert, delete, get use case

---
+++ app/src/main/java/com/example/todosapp/domain/use_case/UpsertTodosUseCase.kt
@@ -0,0 +1,15 @@
+package com.example.todosapp.domain.use_case
+
+import com.example.todosapp.data.repository.TodosRepository
+import com.example.todosapp.domain.model.Todos
+import dagger.hilt.android.scopes.ViewModelScoped
+import javax.inject.Inject
+
+@ViewModelScoped
+class UpsertTodosUseCase @Inject constructor(
+    private val todosRepository: TodosRepository
+) {
+    suspend operator fun invoke(todos: Todos) {
+        return todosRepository.upsertTodos(todos)
+    }
+}
