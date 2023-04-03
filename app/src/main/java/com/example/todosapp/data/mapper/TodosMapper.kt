837665f3e4431aaa8448b36144ede41e4d292a02
exfarnanda1945 <exfarnanda1945@gmail.com>
2023-04-03 11:18:46 +0700

    fix todos mapper at data

--- app/src/main/java/com/example/todosapp/data/mapper/TodosMapper.kt
+++ app/src/main/java/com/example/todosapp/data/mapper/TodosMapper.kt
@@ -4,9 +4,9 @@ import com.example.todosapp.data.local.ToDosEntity
 import com.example.todosapp.domain.model.Todos
 
 fun ToDosEntity.toTodos(): Todos {
-    return Todos(id, title, description, date, priority, isArchive, isDone, deadline)
+    return Todos(id, title, description, priority, isArchive, isDone, date, deadline)
 }
 
 fun Todos.toTodosEntity(): ToDosEntity {
-    return ToDosEntity(id, title, description, date, priority, isArchive, isDone, deadline)
+    return ToDosEntity(id, title, description, priority, isArchive, isDone, date, deadline)
 }
