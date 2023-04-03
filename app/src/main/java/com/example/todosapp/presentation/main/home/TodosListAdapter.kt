37e7856b822062fac6ee62ed25414ad536e4cdc9
exfarnanda1945 <exfarnanda1945@gmail.com>
2023-04-03 11:25:27 +0700

    add todos list adapter

---
+++ app/src/main/java/com/example/todosapp/presentation/main/home/TodosListAdapter.kt
@@ -0,0 +1,89 @@
+package com.example.todosapp.presentation.main.home
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todosapp.R
+import com.example.todosapp.common.Utils
+import com.example.todosapp.data.local.TodosPriority
+import com.example.todosapp.databinding.TodosCardBinding
+import com.example.todosapp.domain.model.Todos
+import com.example.todosapp.presentation.util.ColorInfoTodosCard
+
+class TodosListAdapter : RecyclerView.Adapter<TodosListAdapter.TodosListViewHolder>() {
+
+    private val diffCallBack = object : DiffUtil.ItemCallback<Todos>() {
+        override fun areItemsTheSame(oldItem: Todos, newItem: Todos): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Todos, newItem: Todos): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+    private val differ = AsyncListDiffer(this, diffCallBack)
+
+    inner class TodosListViewHolder(private val binding: TodosCardBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Todos) {
+            binding.titleTodosCard.text = item.title
+            binding.descTodosCard.text = item.description
+            binding.txtPriority.apply {
+                val color = getColorPriority(item.priority)
+                text = item.priority.name
+                setBackgroundColor(ContextCompat.getColor(context, color.backgroundColor))
+                setTextColor(ContextCompat.getColor(context, color.textColor))
+            }
+
+            if (item.deadline != null) {
+                binding.txtDeadline.apply {
+                    text = Utils.convertLongToTime(item.deadline)
+                        .replace(".", " ")
+                    setTextColor(ContextCompat.getColor(context, R.color.red_vermilion))
+                    setBackgroundColor(ContextCompat.getColor(context, R.color.pastel_red))
+                }
+            }else{
+                binding.txtDeadline.apply {
+                    text = context.getString(R.string.no_deadline)
+                    setTextColor(ContextCompat.getColor(context, R.color.may_green))
+                    setBackgroundColor(ContextCompat.getColor(context, R.color.green_alabaster))
+                }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodosListViewHolder {
+        return TodosListViewHolder(
+            TodosCardBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    override fun onBindViewHolder(holder: TodosListViewHolder, position: Int) {
+        holder.bind(differ.currentList[position])
+    }
+
+    private fun getColorPriority(todosPriority: TodosPriority): ColorInfoTodosCard {
+        return when (todosPriority) {
+            TodosPriority.LOW -> ColorInfoTodosCard(R.color.blue, R.color.alice_blue)
+            TodosPriority.MEDIUM -> ColorInfoTodosCard(R.color.marigold, R.color.cosmic_late)
+            TodosPriority.HIGH -> ColorInfoTodosCard(R.color.red_vermilion, R.color.pink_linen)
+        }
+    }
+
+    fun setData(todos: List<Todos>) {
+        differ.submitList(todos)
+    }
+}
