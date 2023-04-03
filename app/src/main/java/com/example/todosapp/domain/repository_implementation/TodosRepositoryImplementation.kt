fb899bd6ba7864fae5220fe36f54a2d7eeca010a
exfarnanda1945 <exfarnanda1945@gmail.com>
2023-04-03 11:13:44 +0700

    change create and update to upsert

--- app/src/main/java/com/example/todosapp/domain/repository_implementation/TodosRepositoryImplementation.kt
+++ app/src/main/java/com/example/todosapp/domain/repository_implementation/TodosRepositoryImplementation.kt
@@ -1,11 +1,11 @@
-package com.example.todosapp.data.repository_implementation
+package com.example.todosapp.domain.repository_implementation
 
-import com.example.todosapp.base.Resource
+import com.example.todosapp.common.Resource
 import com.example.todosapp.data.local.TodosDao
 import com.example.todosapp.data.mapper.toTodos
 import com.example.todosapp.data.mapper.toTodosEntity
+import com.example.todosapp.data.repository.TodosRepository
 import com.example.todosapp.domain.model.Todos
-import com.example.todosapp.domain.repository.TodosRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -16,7 +16,7 @@ import javax.inject.Inject
 class TodosRepositoryImplementation @Inject constructor(
     private val dao: TodosDao
 ) : TodosRepository {
-    override suspend fun createTodos(todos: Todos) {
+    override suspend fun upsertTodos(todos: Todos) {
         try {
             val todosEntity = todos.toTodosEntity()
             dao.createTodos(todosEntity)
@@ -25,31 +25,22 @@ class TodosRepositoryImplementation @Inject constructor(
         }
     }
 
-    override suspend fun getListTodos(): Flow<Resource<List<Todos>>> = flow {
-        emit(Resource.Loading())
+    override fun getListTodos(): Flow<Resource<List<Todos>>> = flow {
         try {
             val getTodos = dao.getListTodos()
             val todos = getTodos.map { it.toTodos() }
             emit(Resource.Success(todos))
         } catch (e: Exception) {
-            emit(Resource.Error(e.localizedMessage ?: "Unexpected error from cache"))
+            emit(Resource.Error(e.localizedMessage ?: "Unexpected error"))
             Timber.d(e)
         }
     }.flowOn(Dispatchers.IO)
 
-    override suspend fun updateTodos(todos: Todos) {
-        try {
-            val todosEntity = todos.toTodosEntity()
-            dao.updateTodos(todosEntity)
-        } catch (e: Exception) {
-            Timber.d(e)
-        }
-    }
 
     override suspend fun deleteTodos(todos: Todos) {
         try {
-            val todosEntity = todos.toTodosEntity()
-            dao.deleteTodos(todosEntity)
+            val toDosEntity = todos.toTodosEntity()
+            dao.deleteTodos(toDosEntity)
         } catch (e: Exception) {
             Timber.d(e)
         }
