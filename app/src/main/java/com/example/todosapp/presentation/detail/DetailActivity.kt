e6bd9606ded5ed62279b0d0d64007baf57825fff
exfarnanda1945 <exfarnanda1945@gmail.com>
2023-04-03 11:22:39 +0700

    add detail activity

---
+++ app/src/main/java/com/example/todosapp/presentation/detail/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.todosapp.presentation.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.todosapp.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
