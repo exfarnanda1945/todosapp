16052b146bcf331007f1a33cb7c2f6d1d923f02f
exfarnanda1945 <exfarnanda1945@gmail.com>
2023-04-05 14:24:21 +0700

    fix flow for get list todos

--- app/src/main/java/com/example/todosapp/data/local/TodosDao.kt
+++ app/src/main/java/com/example/todosapp/data/local/TodosDao.kt
@@ -2,11 +2,12 @@ package com.example.todosapp.data.local
 
 import androidx.room.*
 import com.example.todosapp.common.Constants.TABLE_NAME
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TodosDao {
     @Query("SELECT * FROM $TABLE_NAME WHERE isArchive = 0 AND isDone = 0 ORDER BY date DESC")
-    fun getListTodos(): List<ToDosEntity>
+    fun getListTodos(): Flow<List<ToDosEntity>>
 
     @Upsert
     suspend fun createTodos(todos: ToDosEntity)
