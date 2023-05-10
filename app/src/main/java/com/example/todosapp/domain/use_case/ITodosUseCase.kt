a7f856f867727dfca5b6cdf11a61018a4e0ac533
exfarnanda1945 <exfarnanda1945@gmail.com>
2023-05-10 13:11:14 +0700

    add todos use case

---
+++ app/src/main/java/com/example/todosapp/domain/use_case/ITodosUseCase.kt
@@ -0,0 +1,20 @@
+package com.example.todosapp.domain.use_case
+
+import com.example.todosapp.common.Resource
+import com.example.todosapp.data.local.FilterBy
+import com.example.todosapp.data.local.SortBy
+import com.example.todosapp.domain.model.Todos
+import kotlinx.coroutines.flow.Flow
+
+interface ITodosUseCase {
+    fun getListTodos(): Flow<Resource<List<Todos>>>
+    fun searchAndFilterTodos(
+        filter: FilterBy,
+        sort: SortBy,
+        search: String
+    ): Flow<Resource<List<Todos>>>
+
+    suspend fun upsertTodos(todos: Todos)
+    suspend fun deleteTodos(todos: Todos)
+    suspend fun deleteAllTodos()
+}
---
+++ app/src/main/java/com/example/todosapp/domain/use_case/TodosUseCase.kt
@@ -0,0 +1,40 @@
+package com.example.todosapp.domain.use_case
+
+import com.example.todosapp.common.Resource
+import com.example.todosapp.data.local.FilterBy
+import com.example.todosapp.data.local.SortBy
+import com.example.todosapp.data.repository.TodosRepository
+import com.example.todosapp.domain.model.Todos
+import dagger.hilt.android.scopes.ViewModelScoped
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+@ViewModelScoped
+class TodosUseCase @Inject constructor(
+    private val repository: TodosRepository
+) : ITodosUseCase {
+    override fun getListTodos(): Flow<Resource<List<Todos>>> {
+        return repository.getListTodos()
+    }
+
+    override fun searchAndFilterTodos(
+        filter: FilterBy,
+        sort: SortBy,
+        search: String
+    ): Flow<Resource<List<Todos>>> {
+        return repository.searchAndFilterTodos(filter, sort, search)
+    }
+
+    override suspend fun upsertTodos(todos: Todos) {
+        return repository.upsertTodos(todos)
+    }
+
+    override suspend fun deleteTodos(todos: Todos) {
+        return repository.deleteTodos(todos)
+    }
+
+    override suspend fun deleteAllTodos() {
+        return repository.deleteAllTodos()
+    }
+
+}
